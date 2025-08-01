9b57e06953bc2d500ff23871ef79ba8a6bd1e3aa
Shivani123-max-droid <chughshivani65@gmail.com>
2025-08-02 01:17:34 +0530

    Time: 0 ms (100.00%) | Memory: 97.4 MB (25.51%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxProfit=0,BestBuy=prices[0];
+        for(int i=1;i<prices.size();i++){
+            if(prices[i]>BestBuy){
+                maxProfit=max(maxProfit, prices[i]-BestBuy);
+            }
+            BestBuy=min(BestBuy,prices[i]);
+        }
+        return maxProfit;
+    }
+};
