e3efbe1f038131765b99966ee7775f0ec6912646
Shivani123-max-droid <chughshivani65@gmail.com>
2025-08-06 01:35:20 +0530

    Time: 0 ms (100.00%) | Memory: 15.8 MB (32.95%) - LeetSync

---
+++ 31-next-permutation/next-permutation.cpp
@@ -0,0 +1,54 @@
+// class Solution {
+// public:
+//     void nextPermutation(vector<int>& nums) {
+//         int pivot=-1,n=nums.size();
+//         for(int i=n-2;i>=0;i--){
+//             if(nums[i]<nums[i+1]){
+//                 pivot=i;
+//                 break;
+//             }
+//         }
+//         if(pivot==-1){
+//             reverse(nums.begin(),nums.end());
+//             return;
+//         }
+//         for(int i=n-1;i>pivot;i++){
+//             if(nums[i]>nums[pivot]){
+//                 swap(nums[i],nums[pivot]);
+//                 break;
+//             }
+//         }
+//         reverse(nums.begin()+pivot+1,nums.end());
+//     }
+// };
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int pivot = -1, n = nums.size();
+
+        // Step 1: Find pivot
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                pivot = i;
+                break;
+            }
+        }
+
+        // Step 2: If no pivot, reverse and return
+        if (pivot == -1) {
+            reverse(nums.begin(), nums.end());
+            return;  // ✅ CRITICAL: must return here to avoid out-of-bounds access
+        }
+
+        // Step 3: Swap with next greater element
+        for (int i = n - 1; i > pivot; i--) {
+            if (nums[i] > nums[pivot]) {
+                swap(nums[i], nums[pivot]);
+                break;
+            }
+        }
+
+        // Step 4: Reverse suffix
+        reverse(nums.begin() + pivot + 1, nums.end());
+    }
+};
