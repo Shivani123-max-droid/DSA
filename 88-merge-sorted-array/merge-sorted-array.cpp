e0b532e6291c69627b836b43595aa0ee8cf8c5e3
Shivani123-max-droid <chughshivani65@gmail.com>
2025-08-06 01:03:31 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (70.33%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,10 +1,32 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        for (int j = 0, i = m; j<n; j++){
-            nums1[i] = nums2[j];
-            i++;
+        // this is taking time complexity O((m+n) log(m+n))
+        // for (int j = 0, i = m; j<n; j++){  
+        //     nums1[i] = nums2[j];
+        //     i++;
+        // }
+        // sort(nums1.begin(),nums1.end());
+
+        // this will take O(m+n) time
+        int i=m-1;
+        int j=n-1;
+        int idx=m+n-1;
+        while(i>=0 && j>=0){
+            if(nums1[i]>nums2[j]){
+                nums1[idx]=nums1[i];
+                idx--;
+                i--;
+            } else{
+                nums1[idx]=nums2[j];
+                idx--;
+                j--;
+            }
         }
-        sort(nums1.begin(),nums1.end());
+        while(j>=0){
+            nums1[idx]=nums2[j];
+            idx--;
+            j--;
+        }
     }
 };
