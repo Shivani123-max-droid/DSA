c418e3d4f7c483131f0bf3132693c33c566ff230
Shivani123-max-droid <chughshivani65@gmail.com>
2025-09-11 11:57:07 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (62.77%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -11,21 +11,15 @@
 class Solution {
 public:
     ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
-        ListNode dummy(INT_MIN);
-        ListNode *tail = &dummy;
-        
-        while (l1 && l2) {
-            if (l1->val < l2->val) {
-                tail->next = l1;
-                l1 = l1->next;
-            } else {
-                tail->next = l2;
-                l2 = l2->next;
-            }
-            tail = tail->next;
-        }
-
-        tail->next = l1 ? l1 : l2;
-        return dummy.next;
+       if(l1==NULL || l2==NULL){
+        return l1==NULL? l2:l1;
+       }
+       if(l1->val<=l2->val){
+        l1->next=mergeTwoLists(l1->next,l2);
+        return l1;
+       } else {
+        l2->next=mergeTwoLists(l1,l2->next);
+        return l2;
+       }
     }
 };
